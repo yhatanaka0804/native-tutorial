fb5a0f7d533ecd90279afc300f6464238b4dc987
yhatanaka0804 <yhatanaka0804@gmail.com>
2019-09-24 18:32:13 +0900

    ローカル通知

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-tab1',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+
+  localNotification() {
+    Plugins.LocalNotifications.schedule({
+      notifications: [
+        {
+          id: 1,
+          title: 'ようこそ',
+          body: 'Ionic Frameworkへ',
+          schedule: { at: new Date(Date.now() + 1000 * 5) },
+          sound: 'normail'
+        }
+      ]
+    });
+  }
 
 }
